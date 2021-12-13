2cd00e5910acb6b47b743b8a073168dc1cfaf015
DESKTOP-88FAOV3\EdsoLabs <>
2021-12-13 11:36:52 +0700

    Update redis

--- wise-router/src/main/java/com/edso/wiserouter/controller/SendController.java
+++ wise-router/src/main/java/com/edso/wiserouter/controller/SendController.java
@@ -1,6 +1,7 @@
 package com.edso.wiserouter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,12 @@ public class SendController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private RedisTemplate template;
+
     public void sendMessage(String msg) {
+        template.opsForValue().set("a", msg);
+        System.out.println("Redis " + template.opsForValue().get("a"));
         kafkaTemplate.send("demo", msg);
     }
 
